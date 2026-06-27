04402de3af45b7153ab41e70b08c75e40f9f6068
WEASEL <razesupport@gmail.com>
2026-06-27 16:29:08 +0200

    Delete stub directory

--- stub/snoop_nuke.py
+++
@@ -1,6 +0,0 @@
-# Snoop Nuke Template for SNOOP
-# This is a placeholder. Implement your snoop nuke logic here.
-import os, shutil
-def run_nuke():
-    # Placeholder for destructive functionality
-    pass
